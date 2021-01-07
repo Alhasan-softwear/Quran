15d09897973604abface7f4d80e9b57a4936d052
Alhasan-softwear <69077457+Alhasan-softwear@users.noreply.github.com>
2021-01-07 21:35:25 +0100

    good

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import  Quran from './Quran';
+import {
+  IonApp,
+  IonIcon,
+  IonLabel,
+  IonRouterOutlet,
+  IonTabBar,
+  IonTabButton,
+  IonTabs
+} from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { square, triangle, images } from 'ionicons/icons';
+
+
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+/* Theme variables */
+import './theme/variables.css';
+
+/* Global CSS */
+import './global.css';
+
+
+
+function App() {
+
+return (
+        <div className="App">
+
+<Quran/>
+
+      </div>
+);
+}
+
+
+export default App;
---
+++ src/Quran.js
@@ -0,0 +1,221 @@
+import React from 'react';
+import mp3_file from './quran/114.mp3';
+import mp3_file1 from './quran/113.mp3';
+import mp3_file2 from './quran/112.mp3';
+import mp3_file3 from './quran/112.mp3';
+import mp3_file4 from './quran/112.mp3';
+import mp3_file5 from './quran/111.mp3';
+import mp3_file6 from './quran/110.mp3';
+import mp3_file7 from './quran/109.mp3';
+import mp3_file8 from './quran/108.mp3';
+import mp3_file9 from './quran/107.mp3';
+import mp3_file10 from './quran/106.mp3';
+import mp3_file11 from './quran/105.mp3';
+import mp3_file12 from './quran/104.mp3';
+import mp3_file13 from './quran/103.mp3';
+import mp3_file14 from './quran/102.mp3';
+import mp3_file15 from './quran/101.mp3';
+import mp3_file16 from './quran/100.mp3';
+import mp3_file17 from './quran/099.mp3';
+import mp3_file18 from './quran/098.mp3';
+import mp3_file19 from './quran/097.mp3';
+import mp3_file20 from './quran/096.mp3';
+import mp3_file21 from './quran/114.mp3';
+import mp3_file22 from './quran/114.mp3';
+import mp3_file23 from './quran/114.mp3';
+import mp3_file24 from './quran/114.mp3';
+import mp3_file25 from './quran/114.mp3';
+import mp3_file26 from './quran/114.mp3';
+import mp3_file27 from './quran/114.mp3';
+import mp3_file28 from './quran/114.mp3';
+import mp3_file29 from './quran/114.mp3';
+import mp3_file30 from './quran/114.mp3';
+import mp3_file31 from './quran/114.mp3';
+
+function Quran() {
+  return (
+    <div className="Quran">
+<audio controls>
+  <source src={mp3_file} type="audio/mpeg"></source>
+ 
+  Your browser does not support the audio element.
+           <param name="autoplay" value="true" />
+            <param name="autostart" value="true" />
+</audio>
+<br/>
+<p></p>
+<audio controls>
+  <source src={mp3_file1} type="audio/mpeg"></source>
+ 
+  Your browser does not support the audio element.
+           <param name="autoplay" value="true" />
+            <param name="autostart" value="true" />
+</audio>
+<br/>
+<p></p>
+<audio controls>
+  <source src={mp3_file2} type="audio/mpeg"></source>
+ 
+  Your browser does not support the audio element.
+           <param name="autoplay" value="true" />
+            <param name="autostart" value="true" />
+</audio>
+<br/>
+<p></p>
+<audio controls>
+  <source src={mp3_file3} type="audio/mpeg"></source>
+ 
+  Your browser does not support the audio element.
+           <param name="autoplay" value="true" />
+            <param name="autostart" value="true" />
+</audio>
+<br/>
+<p></p>
+<audio controls>
+  <source src={mp3_file4} type="audio/mpeg"></source>
+ 
+  Your browser does not support the audio element.
+           <param name="autoplay" value="true" />
+            <param name="autostart" value="true" />
+</audio>
+<br/>
+<p></p>
+<audio controls>
+  <source src={mp3_file5} type="audio/mpeg"></source>
+ 
+  Your browser does not support the audio element.
+           <param name="autoplay" value="true" />
+            <param name="autostart" value="true" />
+</audio>
+<br/>
+<p></p>
+<audio controls>
+  <source src={mp3_file6} type="audio/mpeg"></source>
+ 
+  Your browser does not support the audio element.
+           <param name="autoplay" value="true" />
+            <param name="autostart" value="true" />
+</audio>
+<br/>
+<p></p>
+<audio controls>
+  <source src={mp3_file7} type="audio/mpeg"></source>
+ 
+  Your browser does not support the audio element.
+           <param name="autoplay" value="true" />
+            <param name="autostart" value="true" />
+</audio>
+<br/>
+<p></p>
+<audio controls>
+  <source src={mp3_file8} type="audio/mpeg"></source>
+ 
+  Your browser does not support the audio element.
+           <param name="autoplay" value="true" />
+            <param name="autostart" value="true" />
+</audio>
+<br/>
+<p></p>
+<audio controls>
+  <source src={mp3_file9} type="audio/mpeg"></source>
+ 
+  Your browser does not support the audio element.
+           <param name="autoplay" value="true" />
+            <param name="autostart" value="true" />
+</audio>
+<br/>
+<p></p>
+<audio controls>
+  <source src={mp3_file10} type="audio/mpeg"></source>
+ 
+  Your browser does not support the audio element.
+           <param name="autoplay" value="true" />
+            <param name="autostart" value="true" />
+</audio>
+<br/>
+<p></p>
+<audio controls>
+  <source src={mp3_file11} type="audio/mpeg"></source>
+ 
+  Your browser does not support the audio element.
+           <param name="autoplay" value="true" />
+            <param name="autostart" value="true" />
+</audio>
+<br/>
+<p></p>
+<audio controls>
+  <source src={mp3_file12} type="audio/mpeg"></source>
+ 
+  Your browser does not support the audio element.
+           <param name="autoplay" value="true" />
+            <param name="autostart" value="true" />
+</audio>
+<br/>
+<p></p>
+<audio controls>
+  <source src={mp3_file13} type="audio/mpeg"></source>
+ 
+  Your browser does not support the audio element.
+           <param name="autoplay" value="true" />
+            <param name="autostart" value="true" />
+</audio>
+<br/>
+<p></p>
+<audio controls>
+  <source src={mp3_file14} type="audio/mpeg"></source>
+ 
+  Your browser does not support the audio element.
+           <param name="autoplay" value="true" />
+            <param name="autostart" value="true" />
+</audio>
+<br/>
+<p></p>
+<audio controls>
+  <source src={mp3_file15} type="audio/mpeg"></source>
+ 
+  Your browser does not support the audio element.
+           <param name="autoplay" value="true" />
+            <param name="autostart" value="true" />
+</audio>
+<br/>
+<p></p>
+<audio controls>
+  <source src={mp3_file16} type="audio/mpeg"></source>
+ 
+  Your browser does not support the audio element.
+           <param name="autoplay" value="true" />
+            <param name="autostart" value="true" />
+</audio>
+<br/>
+<p></p>
+<audio controls>
+  <source src={mp3_file17} type="audio/mpeg"></source>
+ 
+  Your browser does not support the audio element.
+           <param name="autoplay" value="true" />
+            <param name="autostart" value="true" />
+</audio>
+<br/>
+<p></p>
+<audio controls>
+  <source src={mp3_file18} type="audio/mpeg"></source>
+ 
+  Your browser does not support the audio element.
+           <param name="autoplay" value="true" />
+            <param name="autostart" value="true" />
+</audio>
+<br/>
+<p></p>
+<audio controls>
+  <source src={mp3_file19} type="audio/mpeg"></source>
+ 
+  Your browser does not support the audio element.
+           <param name="autoplay" value="true" />
+            <param name="autostart" value="true" />
+</audio>
+
+    </div>
+  );
+}
+
+export default Quran;
